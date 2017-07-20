20664d61fc676ec46ebbbc3adfe0a746f26d5993
Le Khanh <lekhanhadc@gmail.com>
2017-07-20 13:31:34 +0700

    loading 30%

--- My first big project/UNISEX Salon/js/myJS.js
+++ My first big project/UNISEX Salon/js/myJS.js
@@ -1,5 +1,47 @@
 $(document).ready(function(){
-	$('.owl-carousel').owlCarousel({
+	$('.owl-carousel1').owlCarousel({
+	  items:1,
+	  // autoplay:true,
+	     loop:true,
+	     nav:true,
+	     navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+	     responsive:{
+	         0:{
+	             items:1
+	         },
+	         600:{
+	             items:1
+	         },
+	         1000:{
+	             items:1
+	         }
+	     }
+	 })
+});
+
+$(document).ready(function(){
+	$('.owl-carousel2').owlCarousel({
+	  items:1,
+	  // autoplay:true,
+	     loop:true,
+	     nav:true,
+	     navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+	     responsive:{
+	         0:{
+	             items:1
+	         },
+	         600:{
+	             items:1
+	         },
+	         1000:{
+	             items:1
+	         }
+	     }
+	 })
+});
+
+$(document).ready(function(){
+	$('.owl-carousel3').owlCarousel({
 	  items:1,
 	  // autoplay:true,
 	     loop:true,
@@ -13,8 +55,48 @@ $(document).ready(function(){
 	             items:3
 	         },
 	         1000:{
-	             items:1
+	             items:4
 	         }
 	     }
 	 })
+});
+
+$(document).ready(function(){
+    $('.owl-carousel4').owlCarousel({
+        items:1,
+        // autoplay:true,
+        loop:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+});
+
+$(document).ready(function(){
+    $('.owl-carousel5').owlCarousel({
+        items:1,
+        // autoplay:true,
+        loop:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
 });
