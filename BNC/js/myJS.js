d60fa96df66dea8bf25072003046faf81ef0adc5
Le Khanh <lekhanhadc@gmail.com>
2017-07-30 08:47:37 +0700

    Updata project BNC

---
+++ BNC/js/myJS.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    margin:10,
+    responsiveClass:true,
+    autoplay:true,
+    autoplayTimeout:1300,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:true
+        }
+    }
+})
